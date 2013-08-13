40af8ddf90ec635fd790ab26e1f5dbc823b679f0
Isaac Grant <waterwolf2.0@gmail.com>
2013-08-12 19:28:15 -0600

    removed unused imports

--- src/com/isaacjg/darklight/issues/PathIssue.java
+++ src/com/isaacjg/darklight/issues/PathIssue.java
@@ -1,9 +1,5 @@
 package com.isaacjg.darklight.issues;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import com.ijg.darklight.sdk.core.Issue;
 
 /*
